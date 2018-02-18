1b69c3cd36fa7c0f91c8bff67ce700021462dc18
Fireflew <gordi@tenev.com>
2018-02-18 17:34:16 -0600

    i am showing how to do this stuff

--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("starting a new chess game!");
         Game chess = new Game();
-        //if youre seeing this i was able to edit it from intellij
+        //if youre seeing this i was able to edit it from intellij!!!! yeet woohooo
     }
 }
