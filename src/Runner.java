5c42179d88deb601c29f14e7b4c636f98014146f
Fireflew <gordi@tenev.com>
2018-02-18 17:29:44 -0600

    test

--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("starting a new chess game!");
         Game chess = new Game();
+        //if youre seeing this my pull request attempt worked!!!
     }
 }
