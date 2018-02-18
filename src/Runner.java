308d78dcf46d1112556c8ba7f68bf26d226a33a1
Fireflew Development <gordi@tenev.com>
2018-02-18 17:51:50 -0600

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("starting a new chess game!");
         Game chess = new Game();
-        //trying to get intellij to be updated now...
+        //cool and good
     }
 }
