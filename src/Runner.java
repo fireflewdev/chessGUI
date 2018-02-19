409fea34a879bef6e79af46f2a49f0c17cd58ec5
DJN1 <34351129+DJN1@users.noreply.github.com>
2018-02-19 08:38:02 -0600

    made private

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,5 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("starting a new chess game!");
         Game chess = new Game();
-        //cool and good
-        //very nice
     }
 }
