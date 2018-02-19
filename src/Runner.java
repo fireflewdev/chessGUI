48e71452609f2fb07c67edad5eb92d7e0018f68e
DJN1 <34351129+DJN1@users.noreply.github.com>
2018-02-18 21:54:54 -0600

    testing

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,5 @@
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         System.out.println("starting a new chess game!");
         Game chess = new Game();
 
