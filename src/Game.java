d02944ef7794865ddcf631bbc413544ef15ac32d
DJN1 <34351129+DJN1@users.noreply.github.com>
2018-02-18 18:28:50 -0600

    added 2d array for board

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,12 @@
 public class Game {
+    private int[][] board;
     /*
      * What should be here:
      * -8x8 array with board pieces
      * -GUI
      * -An interface to interact with the board
      */
+    public Game() {
+        board = new int[8][8];
+    }
 }
