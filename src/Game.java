d369559931f3ad03eacb1914c88e80d09375ffe0
DJN1 <34351129+DJN1@users.noreply.github.com>
2018-02-18 19:27:35 -0600

    initialized board; paint()

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,9 @@
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JPanel{
     private int[][] board;
+
     /*
      * What should be here:
      * -8x8 array with board pieces
@@ -8,5 +12,28 @@ public class Game {
      */
     public Game() {
         board = new int[8][8];
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board.length; j++) {
+                board[i][j] = 0;//0 = empty
+            }
+        }
+
+    }
+
+
+    public void paint(Graphics g){
+        g.fillRect(100, 100, 400, 400);
+        for(int i = 100; i <= 400; i+=100){
+            for(int j = 100; j <= 400; j+=100){
+                g.clearRect(i, j, 50, 50);
+            }
+        }
+
+        for(int i = 150; i <= 450; i+=100){
+            for(int j = 150; j <= 450; j+=100){
+                g.clearRect(i, j, 50, 50);
+            }
+        }
     }
 }
+
